406d9fbf17f72348a4c0ff4b09049e2217e6ce36
Juan Najera <jcnajeradev@gmail.com>
2025-03-15 00:50:29 -0500

    Consolidation of JSON response

--- home.js
+++ home.js
@@ -5,45 +5,48 @@ import colors from "colors";
 let response = Router();
 
 let valueObject = {
-  cpu: "manufacturer, brand, processors, physicalCores, speed, speedMax",
-  osInfo: "platform, release",
-  system: "model, manufacturer",
+	cpu: "manufacturer, brand, processors, physicalCores, speed, speedMax",
+	osInfo: "platform, release",
+	system: "model, manufacturer",
 };
 
 const general = await si
-  .get(valueObject)
-  .then((data) => {
-    return data;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+	.get(valueObject)
+	.then((data) => {
+		return data;
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 
 const memory = await si
-  .mem()
-  .then((data) => {
-    return data;
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+	.mem()
+	.then((data) => {
+		return data;
+	})
+	.catch((error) => {
+		console.log(error);
+	});
 
 response.get("/", (req, res) => {
-  //   console.log(general);
-  //   const { cpu } = general;
-  //   console.log(cpu);
-  //   console.log(memory);
+	//   console.log(general);
+	//   const { cpu } = general;
+	//   console.log(cpu);
+	//   console.log(memory);
 
-  let merged = { ...general, ...memory };
+	let merged = { ...general, ...memory };
 
-  let { total, free, used } = memory;
-  total = total / 1024 / 1024;
-  free = (free / 1024 / 1024).toFixed(1);
-  used = (used / 1024 / 1024).toFixed(1);
-  let memoryTotal = { total, free, used };
-  console.log(memoryTotal);
+	let { total, free, used } = memory;
+	total = total / 1024 / 1024;
+	free = Number((free / 1024 / 1024).toPrecision(6));
+	used = Number((used / 1024 / 1024).toPrecision(6));
+	let memoryTotal = { total, free, used };
+	// console.log(memoryTotal);
 
-  //   console.log(merged);
+	merged = { ...general, ...memoryTotal };
+	console.log(merged);
+
+	//   console.log(merged);
 });
 
 export default response;
